b9e8c2631132fa76bb603a5930f7fb22dc5eee33
Alberto González <alberto.gonzalez@distillery.com>
2022-03-30 11:07:28 -0600

    The initial set up server was implemented.

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+//Routes
+app.get("/hello", (req, res) => {
+  res.send("Task manager app");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
